1d3cd588d1fe07264e3fa248bf48a00ad90d023a
fr-xiefan <fan.xie@wearex.com>
2017-10-30 09:14:31 +0900

    responsive

--- client/src/components/appFooter.vue
+++ client/src/components/appFooter.vue
@@ -1,11 +1,11 @@
 <template>
   <footer class="main-footer">
-      <div class="container">
-          <div class="pull-right hidden-xs">
-              <b>Version</b> 1.0.0
-          </div>
-          <strong>Copyright &copy; 2017 <a href="http://www.miraimon.com/">Miraimon</a>.</strong> All rights reserved.
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Version</b> 1.0.0
       </div>
+      <strong>Copyright &copy; 2017 <a href="http://www.miraimon.com/">Miraimon</a>.</strong> All rights reserved.
+    </div>
   </footer>
 </template>
 
